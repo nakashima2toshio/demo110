67a98c79d3831880732a73b81940d858e4bb66e9
nakashima2toshio <nakashima2toshio@gmail.com>
2018-05-19 13:55:50 +0900

    change name

--- src/main/java/com/example/demo/Demo110Application.java
+++ src/main/java/com/example/demo/Demo110Application.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Demo20Application {
+public class Demo110Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo20Application.class, args);
+		SpringApplication.run(Demo110Application.class, args);
 	}
 }
