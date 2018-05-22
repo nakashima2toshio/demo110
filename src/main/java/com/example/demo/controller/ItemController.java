da2fa1945d0fb117c5b7d6ba7b7f516c086028c2
nakashima2toshio <nakashima2toshio@gmail.com>
2018-05-22 22:59:09 +0900

    5-22

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -6,22 +6,32 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Item;
 import com.example.demo.service.ItemService;
 
 @Controller
-// @RequestMapping("/items")
+@RequestMapping("/items")
 public class ItemController {
 
 	@Autowired
 	private ItemService itemService;
 	
-	@GetMapping("/items")
+	@RequestMapping(method = RequestMethod.GET)
 	public String index(Model model) {
 		List<Item> items = itemService.findAll();
 		model.addAttribute("items", items);
 		return "items/index";
 	}
-	
+	/*
+	@RequestMapping(method=RequestMethod.GET, value="/items/{id}")
+	public Item getItem(@PathVariable("id") Long id) {
+		Item itm =  itemService.findOne(id);
+		return Item;
+	}
+	*/
 }
